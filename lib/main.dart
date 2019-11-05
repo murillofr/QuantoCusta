f220c6472047848829cdce215d443e8c45d932a7
Murillo Falcao <murillo.falcao@gmail.com>
2019-11-05 17:57:06 -0300

    Inicio do desenvolvimento do leitor de código de barras

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -18,6 +21,40 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   int _page = 0;
   GlobalKey _bottomNavigationKey = GlobalKey();
+  String _scanBarcode = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            "#ff6666", "Cancel", true, ScanMode.BARCODE)
+        .listen((barcode) => print(barcode));
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on Exception {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,28 +76,52 @@ class _BottomNavBarState extends State<BottomNavBar> {
         animationDuration: Duration(milliseconds: 500),
         onTap: (index) {
           setState(() {
-            _page = index;
+            if (_page != index) {
+              _page = index;
+            }
           });
         },
       ),
       body: Container(
         color: Colors.blueAccent,
         child: Center(
-          child: Column(
-            children: <Widget>[
-              Text(_page.toString(), textScaleFactor: 10.0),
-              RaisedButton(
-                child: Text('Go To Page of index 1'),
-                onPressed: () {
-                  final CurvedNavigationBarState navBarState =
-                      _bottomNavigationKey.currentState;
-                  navBarState.setPage(1);
-                },
-              )
-            ],
-          ),
+          child: _switcherBody(),
         ),
       ),
     );
+  }
+
+  Widget _switcherBody() {
+    switch (_page) {
+      case 0:
+        return new Container(child: Center(child: new Text("Envio de erros")));
+      case 1:
+        return new Container(child: Center(child: new Text("Cupons")));
+      case 2:
+        return new Container(
+          alignment: Alignment.center,
+          child: Flex(
+            direction: Axis.vertical,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () => scanBarcodeNormal(),
+                child: Text("Start barcode scan"),
+              ),
+              RaisedButton(
+                onPressed: () => startBarcodeScanStream(),
+                child: Text("Start barcode scan stream"),
+              ),
+              Text(
+                'Scan result : $_scanBarcode\n',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        );
+      case 3:
+        return new Container(child: Center(child: new Text("Lista de produtos")));
+    }
+    return null;
   }
 }
